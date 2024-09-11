cd02daa3c5fbeac6bcabc516f6a66fb252e47eb5
lucassamelFadami <lucas.samel@fadami.com.br>
2024-09-11 14:47:03 -0300

    Models

---
+++ Models/Post.cs
@@ -0,0 +1,31 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blog.Models
+{
+    [Table("[Post]")]
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Body { get; set; }
+        public string Slug { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+
+        public int AuthorId { get; set; }
+        [Write(false)]
+        public User Author { get; set; }
+
+        public int CategoryId { get; set; }
+        [Write(false)]
+        public Category Category { get; set; }
+
+        [Write(false)]
+        public List<Tag> Tags { get; set; }
+    }
